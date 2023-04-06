a8582dd46628a15660bc9c2ea57ed7c1be0f95fa
jakeszhang <jakeszhang@qt-asia.com>
2023-04-06 17:19:20 +0800

    yh

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,16 +1,52 @@
 <template>
-    <div class="login">
-        <div>
-            <p>开发调试登陆</p>
-            <el-input
-                placeholder="请输入ssid"
-                size="medium"
-                v-model="ssid"
-                @keyup.enter.native="getSsid"
-            />
-            <el-button type="primary" size="medium" :loading="loading" @click="getSsid">
-                获取SSID
-            </el-button>
+    <div id="syApp">
+        <div class="ln_page" :style="{ backgroundImage: `url(${backgroundImgUrl})` }">
+            <div class="ln_topLogo">
+                <img src="@/assets/images/logofff.png" style="height: 12vh;" />
+            </div>
+            <div class="ln_content">
+                <div class="login_form">
+                    <div class="title">流域暴雨洪水感知调控虚拟仿真实验</div>
+                    <div class="title2">用户登录</div>
+                    <div class="login_input">
+                        <img src="@/assets/images/email.png" />
+                        <input type="email" v-model="user" placeholder="请输入账号" />
+                    </div>
+                    <div class="login_input">
+                        <img src="@/assets/images/pass.png" />
+                        <input type="password" v-model="pass" placeholder="请输入密码" />
+                    </div>
+                    <button @click="login()">登 录</button>
+                    <p>
+                        还没有账号？去
+                        <!-- <a href="register.html">注册</a> -->
+                        <router-link to="register">注册</router-link>
+                    </p>
+                    <div class="login_enter" @click="jump">专家入口 ></div>
+                </div>
+            </div>
+        </div>
+
+        <!-- 提示登录 -->
+        <div class="ln_tip" :class="{ ln_tipOut: showTip }">
+            <div class="ln_TMark"></div>
+            <div class="ln_TPack">
+                <div class="ln_TPClose" @click="showTip = false">
+                    <img src="@/assets/images/close_icon.png" alt="" />
+                </div>
+                <div class="ln_TPTitle">友情提示</div>
+                <p>
+                    1、如果需要将此次实验数据上传至ilab实验空间，请务必点击下方按钮“
+                    <a :href="ilabLoginHref">去实验空间登录</a>
+                    ”。
+                </p>
+                <p>
+                    2、如果只是想体验一下实验，并不需要将此次实验数据上传至ilab实验空间，点击右上角“
+                    x ”忽略此弹框。
+                </p>
+
+                <a class="ln_TPSubmit" :href="ilabLoginHref">去实验空间登录</a>
+            </div>
         </div>
     </div>
 </template>
@@ -20,27 +56,56 @@ import { mapActions } from 'vuex';
 
 export default {
     name: 'Login',
-
     data() {
         return {
-            ssid: '',
-            loading: false
+            user: '',
+            pass: '',
+            ticket: '',
+            showTip: false,
+            ilabLoginHref: 'http://www.ilab-x.com/login', // ?ref=%2Fdetails%2Fv5%3Fid%3D7033%26isView%3Dtrue
+            backgroundImgUrl: require('@/assets/images/loginback.png')
         };
     },
-
+    mounted() {
+        // var ticket = $sy.getUrlData("ticket");
+        // if(ticket == undefined){
+        //     this.showTip = true;
+        // }
+        // this.ticket = ticket;
+        document.onkeydown = function (e) {
+            let ev = document.all ? window.event : e;
+            if (ev.keyCode == 13) {
+                this.login();
+            }
+        };
+    },
     methods: {
         ...mapActions('common', ['setData']),
 
-        getSsid() {
-            if (this.ssid) {
-                this.setData({ key: 'ssid', value: this.ssid, modules: 'common' });
-                this.$router.push('/');
-            } else {
+        async login() {
+            if (this.user == '' || this.pass == '') {
                 this.$notify.error({
                     title: '错误',
-                    message: 'ssid不正确'
+                    message: '账号与密码不能为空'
                 });
+                return;
             }
+            const res = await this.$http.fetchData({
+                url: 'SCM.TMS7.WebApi/Oauth/GetSsidFormCurrentContext',
+                params: {
+                    pass: this.pass,
+                    user: this.user
+                },
+                type: 2
+            });
+            this.setData({ key: 'token', value: res.token, modules: 'common' });
+            this.$router.push('index');
+        },
+        jump() {
+            this.vp_login('1070824', '1070824');
+        },
+        goRegister() {
+            this.$router.push('register');
         }
     }
 };
