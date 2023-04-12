52daf952a363d11b648259813ab037fcd902032a
jakeszhang <jakeszhang@qt-asia.com>
2023-04-12 11:32:32 +0800

    新增

--- src/views/home/components/laboratory.vue
+++ src/views/home/components/laboratory.vue
@@ -1,7 +1,26 @@
 <template>
     <div>
         <div id="pdfDom">
-            页面内容
+            <table border="1" cellspacing="0" style="border-color: gray; color: gray;">
+                <tr>
+                    <td>Month</td>
+                    <td>Savings</td>
+                    <td>Month</td>
+                    <td>Savings</td>
+                </tr>
+                <tr>
+                    <td>January</td>
+                    <td>$100</td>
+                    <td>Month</td>
+                    <td>Savings</td>
+                </tr>
+            </table>
+
+            <el-table :data="tableData" border style="width: 100%;">
+                <el-table-column prop="date" label="日期" width="180"></el-table-column>
+                <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+                <el-table-column prop="address" label="地址"></el-table-column>
+            </el-table>
         </div>
         <Footer />
     </div>
@@ -16,7 +35,30 @@ export default {
         Footer
     },
     data() {
-        return {};
+        return {
+            tableData: [
+                {
+                    date: '2016-05-02',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                },
+                {
+                    date: '2016-05-04',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1517 弄'
+                },
+                {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1519 弄'
+                },
+                {
+                    date: '2016-05-03',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1516 弄'
+                }
+            ]
+        };
     },
 
     methods: {
