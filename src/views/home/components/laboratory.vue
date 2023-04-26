23a88e3003893289b8fc2937f39266696ecd0ec8
jakeszhang <jakeszhang@qt-asia.com>
2023-04-26 15:39:37 +0800

    优化

--- src/views/home/components/laboratory.vue
+++ src/views/home/components/laboratory.vue
@@ -1,26 +1,28 @@
 <template>
     <div>
         <div id="pdfDom">
-            <table border="1" cellspacing="0" style="border-color: gray; color: gray;">
-                <tr>
-                    <td>Month</td>
-                    <td>Savings</td>
-                    <td>Month</td>
-                    <td>Savings</td>
-                </tr>
-                <tr>
-                    <td>January</td>
-                    <td>$100</td>
-                    <td>Month</td>
-                    <td>Savings</td>
-                </tr>
-            </table>
+            <div v-for="(item, index) in list" :key="index">
+                <table border="1" cellspacing="0" style="border-color: gray; color: gray;">
+                    <tr>
+                        <td>Month</td>
+                        <td>Savings</td>
+                        <td>Month</td>
+                        <td>Savings</td>
+                    </tr>
+                    <tr>
+                        <td>January</td>
+                        <td>$100</td>
+                        <td>Month</td>
+                        <td>Savings</td>
+                    </tr>
+                </table>
 
-            <el-table :data="tableData" border style="width: 100%;">
-                <el-table-column prop="date" label="日期" width="180"></el-table-column>
-                <el-table-column prop="name" label="姓名" width="180"></el-table-column>
-                <el-table-column prop="address" label="地址"></el-table-column>
-            </el-table>
+                <el-table :data="item.experimentsteppanel.steps" border style="width: 100%;">
+                    <el-table-column prop="date" label="名称" width="180"></el-table-column>
+                    <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+                    <el-table-column prop="address" label="地址"></el-table-column>
+                </el-table>
+            </div>
         </div>
         <Footer />
     </div>
@@ -36,34 +38,23 @@ export default {
     },
     data() {
         return {
-            tableData: [
-                {
-                    date: '2016-05-02',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
-                },
-                {
-                    date: '2016-05-04',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1517 弄'
-                },
-                {
-                    date: '2016-05-01',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1519 弄'
-                },
-                {
-                    date: '2016-05-03',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1516 弄'
-                }
-            ]
+            list: [],
+            tableData: []
         };
     },
-
+    mounted() {
+        this.getReportInfo();
+    },
     methods: {
         pdfBtn() {
             htmlToPdf.getPdf('页面导出PDF文件名');
+        },
+        async getReportInfo() {
+            const res = await this.$http.fetchData({
+                url: '/vr/experimentController/getReportInfo',
+                type: 2
+            });
+            this.list = res.data;
         }
     }
 };
